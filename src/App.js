5eb3cda3abdce47ad1341a786603ce181f332cc4
Nirakar <goswaminirakar65@gmail.com>
2024-11-11 10:44:43 +0530

    "ok"

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
+import REFUND from "./components/Refund/Refund"
 import Resume from "./components/Resume/ResumeNew";
 import Form from "./components/Form/form"
 import Signup from "./components/Autentication/Signup";
@@ -13,6 +14,7 @@ import Tempalte from "./components/Template/Templates"
 import Contect from "./components/contectus"
 import Consent from "./components/Concent/Conecent"
 import Serchfrom from "./components/SercehBar/serchbar"
+import TermsOfService from "./components/Teremuse"
 import {
   BrowserRouter as Router,
   Route,
@@ -60,6 +62,9 @@ function App() {
           {/* <Route path="/resume" element={<Resume />} /> */}
           <Route path="/template" element={<Tempalte />} />
           <Route path="/form" element={<Form />} />
+          <Route path="/termsOfService" element={<TermsOfService />} />
+          <Route path="/Refund" element={<REFUND />} />
+
           {/* <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} /> */}
 
---
+++ src/components/Refund/Refund.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+
+const REFUND = () => {
+    return (
+        <div>
+            <h1>
+                Cancellation & Refund Policy
+            </h1>
+            <div>
+                <h2>
+                    Last updated on 11-11-2024 10:38:25
+                </h2>
+
+            </div>
+            <div>
+                NIRAKARGIRI NARENDRAGIRI GOSVAMI believes in helping its customers as far as possible, and
+                has therefore a liberal cancellation policy. Under this policy:
+            </div>
+            <p>
+                • Cancellations will be considered only if the request is made immediately after placing the order.
+                However, the cancellation request may not be entertained if the orders have been communicated to the
+                vendors/merchants and they have initiated the process of shipping them.
+            </p>
+            <p>
+                • NIRAKARGIRI NARENDRAGIRI GOSVAMI does not accept cancellation requests for perishable
+                items like flowers, eatables etc. However, refund/replacement can be made if the customer establishes
+                that the quality of product delivered is not good
+            </p>
+            <p>
+                In case of receipt of damaged or defective items please report the same to our Customer Service team.
+                The request will, however, be entertained once the merchant has checked and determined the same at his
+                own end. This should be reported within 7 Days days of receipt of the products. In case you feel that the
+                product received is not as shown on the site or as per your expectations, you must bring it to the notice of
+                our customer service within 7 Days days of receiving the product. The Customer Service Team after
+                looking into your complaint will take an appropriate decision.
+            </p>
+            <p>
+                In case of complaints regarding products that come with a warranty from manufacturers, please refer
+                the issue to them. In case of any Refunds approved by the NIRAKARGIRI NARENDRAGIRI
+                GOSVAMI, it’ll take 1-2 Days days for the refund to be processed to the end customer.
+            </p>
+        </div>
+
+    )
+}
+
+export default REFUND
