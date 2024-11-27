aee42f4e527e8a1db35dc7260b12fef543b39080
Nirakar <goswaminirakar65@gmail.com>
2024-11-27 13:53:26 +0530

    shippign

--- src/components/Refund/Refund.js
+++ src/components/Refund/Refund.js
@@ -65,6 +65,15 @@ const RefundAndCancellationPolicy = () => {
         customer service team. We’re here to help resolve your concerns as
         quickly as possible.
       </p>
+
+      <h2>
+      Shipping Policy
+      </h2>
+      <p>
+      The orders for the user are shipped through registered domestic courier companies and/or speed post only. Orders are shipped within 7 days from the date of the order and/or payment or as per the delivery date agreed at the time of order confirmation and delivering of the shipment, subject to courier company / post office norms.
+      <br/>
+       Platform Owner shall not be liable for any delay in delivery by the courier company / postal authority. Delivery of all orders will be made to the address provided by the buyer at the time of purchase. Delivery of our services will be confirmed on your email ID as specified at the time of registration. If there are any shipping cost(s) levied by the seller or the Platform Owner (as the case be), the same is not refundable.
+      </p>
     </div>
   );
 };
